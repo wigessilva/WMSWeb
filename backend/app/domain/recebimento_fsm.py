2d5dd3db729555f6f5761a7928a5efc1ceea31fe
Willian Silva <williangeorges@gmail.com>
2026-04-15 23:56:53 -0300

    corrige maquina de estados para permitir parcial

--- backend/app/domain/recebimento_fsm.py
+++ backend/app/domain/recebimento_fsm.py
@@ -34,7 +34,7 @@ class RecebimentoFSM(object):
         self.machine.add_transition(trigger='iniciar_conferencia', source=['AGUARDANDO_CONFERENCIA', 'EM_CONFERENCIA', 'LIBERADO'], dest='EM_CONFERENCIA')
         self.machine.add_transition(trigger='cancelar_conferencia', source=['AGUARDANDO_CONFERENCIA', 'EM_CONFERENCIA'], dest='AGUARDANDO_LIBERACAO')
         self.machine.add_transition(trigger='rejeitar', source='*', dest='REJEITADO', unless=['is_finalizado'])
-        self.machine.add_transition(trigger='concluir_parcial', source=['EM_CONFERENCIA', 'AGUARDANDO_CONFERENCIA'], dest='PARCIAL')
+        self.machine.add_transition(trigger='concluir_parcial', source=['EM_CONFERENCIA', 'AGUARDANDO_CONFERENCIA', 'EM_ANALISE'], dest='PARCIAL')
         self.machine.add_transition(trigger='concluir', source=['EM_CONFERENCIA', 'AGUARDANDO_CONFERENCIA', 'EM_ANALISE'], dest='FINALIZADO')
 
     def is_finalizado(self):
